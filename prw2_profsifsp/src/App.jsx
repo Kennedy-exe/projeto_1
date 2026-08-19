f34ded866ac6080d4a5ae26a2aa0073343a52436
Kennedy-exe <kennedyrios99@gmail.com>
2026-08-19 00:30:11 -0300

    aula 3 - componentes Botao, Lista, ListaSuspensa e Formulario

--- prw2_profsifsp/src/App.jsx
+++ prw2_profsifsp/src/App.jsx
@@ -1,18 +1,24 @@
-
-import './App.css'
-import Banner from'./componentes/Banner/Banner';
-import Formulario from './componentes/Formulario/Formulario';
-
-
+import React, { useState } from "react";
+import Banner from "./componentes/Banner/Banner";
+import Formulario from "./componentes/Formulario/Formulario";
+import Lista from "./componentes/Lista/Lista";
 function App() {
+// lista (array) de professores.
+// o valor inicial é um array vazio []
+    const [professores, setProfessores] = useState( [] )
+    const adicionaProf = (prof) => {
+      const listaNova = professores.slice();
+      listaNova.push(prof)
+      setProfessores( listaNova )
 
-
-  return (
-    <div>
-      <Banner/>
-      <Formulario />
-    </div>
-  );
+    console.log(prof)
 }
-      
-export default App
+return (
+  <div>
+    <Banner />
+    <Formulario aoProfCadastrado={prof => adicionaProf(prof)} />
+    <Lista objetos={professores} />
+  </div>
+);
+}
+export default App;
